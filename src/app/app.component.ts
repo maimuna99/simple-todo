7e649742ddc5b8564740813a979b6d70ee4ddb4e
Your Name <you@example.com>
2023-05-26 02:42:17 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { TodoItem } from './app.models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'to-do';
+  TodoItems: TodoItem[] = [
+    {
+      title: 'mimi',
+      isCompleted: false,
+      id: 1,
+    },
+    {
+      title: 'lara',
+      isCompleted: false,
+      id: 2,
+    },
+    {
+      title: 'rahaf',
+      isCompleted: false,
+      id: 3,
+    },
+  ];
+
+  addItem(): void {
+    this.TodoItems.push({
+      title: this.title,
+      id: Date.now(),
+      isCompleted: false,
+    });
+    this.title = '';
+  }
+
+  toggleTodoItem(todoItem: TodoItem): void {
+    this.TodoItems = this.TodoItems.map((item) => {
+      if (item.id === todoItem.id) {
+        return {
+          ...item,
+          isCompleted: !item.isCompleted,
+        };
+      }
+      return item;
+    });
+  }
 }
---
+++ src/app/app.models.ts
@@ -0,0 +1,17 @@
+
+/**
+ * 
+ * This is To do item
+ */
+export  interface TodoItem{
+
+    title: string;
+    isCompleted: boolean;
+    id:number;
+}
+
+const item: TodoItem ={
+    title: 'foo',
+    isCompleted: false,
+    id:0,
+}
